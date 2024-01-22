954a017d0743d84415d3f114e542bf2554304672
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2024-01-22 19:10:15 +0100

    feat: light switch

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,18 @@
 <script>
+	import LightSwitch from '$lib/components/light-switch.svelte';
 	import { Toaster } from '$shadcn/sonner';
+	import { ModeWatcher } from 'mode-watcher';
 	import '../app.pcss';
 </script>
 
 <Toaster richColors />
+<ModeWatcher />
 
 <div class="h-full w-full">
+	<div class="absolute m-3">
+		<LightSwitch />
+	</div>
+
 	<main class="container h-full">
 		<slot />
 	</main>
