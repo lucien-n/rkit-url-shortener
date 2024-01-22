a9d0399f00a8645745b2a739d0e515ceed4dc49d
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2024-01-22 21:33:03 +0100

    feat: slight redesign of most viewed urls

--- src/lib/components/most-viewed.svelte
+++ src/lib/components/most-viewed.svelte
@@ -6,38 +6,45 @@
 	import { Link2 } from 'radix-icons-svelte';
 
 	export let mostViewedUrls: Url[];
+
+	const formatCount = (count: number) => {
+		const formatter = Intl.NumberFormat('en', { notation: 'compact' });
+		return formatter.format(count);
+	};
 </script>
 
-<div class="text-center text-lg font-bold">
-	<p>Most viewed urls</p>
-	<div class="grid grid-cols-2 items-center">
+<div>
+	<p class="text-center text-lg font-bold">Most viewed urls</p>
+	<div class="grid grid-cols-2 items-center gap-x-8">
 		{#each mostViewedUrls as url, index (url.id)}
-			{#if browser}
-				{@const href = browser ? window.location.origin + '/' + url.tinyId : 'l'}
-				<Button
-					{href}
-					variant="link"
-					data-sveltekit-preload-data="off"
-					class={cn('mx-auto flex items-center gap-1', index === 2 ? 'col-span-2 mx-auto' : '')}
-				>
-					<Link2 />
-					{href}
-					<p
-						class="mx-1 flex aspect-square h-full items-center justify-center rounded bg-primary-foreground px-1 font-bold"
+			<div class={cn('mx-auto flex items-center gap-1', index === 2 ? 'col-span-2 mx-auto' : '')}>
+				{#if browser}
+					{@const href = browser ? window.location.origin + '/' + url.tinyId : 'l'}
+					<Button
+						{href}
+						variant="link"
+						data-sveltekit-preload-data="off"
+						class="mx-auto flex items-center gap-1 px-1"
 					>
-						{url.redirects}
+						<Link2 />
+						{href}
+					</Button>
+					<p
+						class="flex aspect-square h-5 items-center justify-center rounded bg-primary-foreground px-1 text-sm font-bold text-primary"
+					>
+						{formatCount(url.redirects)}
 					</p>
-				</Button>
-			{:else}
-				<Button
-					variant="link"
-					data-sveltekit-preload-data="off"
-					class={cn('mx-auto flex items-center gap-1', index === 2 ? 'col-span-2 mx-auto' : '')}
-				>
-					<Link2 />
-					<p class="h-3 w-52 animate-pulse rounded bg-primary/60" />
-				</Button>
-			{/if}
+				{:else}
+					<Button
+						variant="link"
+						data-sveltekit-preload-data="off"
+						class={cn('mx-auto flex items-center gap-1', index === 2 ? 'col-span-2 mx-auto' : '')}
+					>
+						<Link2 />
+						<p class="h-3 w-52 animate-pulse rounded bg-primary/60" />
+					</Button>
+				{/if}
+			</div>
 		{/each}
 	</div>
 </div>
