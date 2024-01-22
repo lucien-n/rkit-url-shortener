7882cb36f9d86bbefbe4f76ecf9cd697f048107d
lucien-neuhoff <lucien.neuhoff@everping.eu>
2024-01-22 10:49:40 +0100

    feat: url dialog copy and display url

--- src/lib/components/url-dialog.svelte
+++ src/lib/components/url-dialog.svelte
@@ -2,23 +2,35 @@
 	import { copyToClipboard } from '$lib/utils';
 	import { Button } from '$shadcn/button';
 	import * as Dialog from '$shadcn/dialog';
+	import { ClipboardCopy } from 'radix-icons-svelte';
 
 	export let open = false;
 	export let shortenedUrl: string;
+
+	let copySucceeded = false;
 </script>
 
 <Dialog.Root bind:open>
 	<Dialog.Content class="sm:max-w-[425px]">
 		<Dialog.Header>
-			<Dialog.Title>Shortened url</Dialog.Title>
-			<Dialog.Description>Here'es your shortened url!</Dialog.Description>
+			<Dialog.Title>Success!</Dialog.Title>
+			<Dialog.Description>Here's your shortened url!</Dialog.Description>
+			<Button variant="link" href={shortenedUrl}>{shortenedUrl}</Button>
 		</Dialog.Header>
 
 		<Dialog.Footer>
-			<Button variant="secondary" type="submit">Close</Button>
-			<Button on:click={() => copyToClipboard(shortenedUrl, () => console.log('Success'))}
-				>Copy</Button
-			>
+			<Button variant="secondary" on:click={() => (open = false)}>Close</Button>
+			<Button
+				class="flex items-center gap-1"
+				on:click={() =>
+					copyToClipboard(shortenedUrl, () => {
+						copySucceeded = true;
+					})}
+				>{copySucceeded ? 'Copied!' : 'Copy'}
+				{#if !copySucceeded}
+					<ClipboardCopy />
+				{/if}
+			</Button>
 		</Dialog.Footer>
 	</Dialog.Content>
 </Dialog.Root>
