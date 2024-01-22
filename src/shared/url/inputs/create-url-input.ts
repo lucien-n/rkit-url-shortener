bfb69c84e97995a42d122b78347024a4eb22b346
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2024-01-22 20:34:32 +0100

    feat: typed url as zod url

--- src/shared/url/inputs/create-url-input.ts
+++ src/shared/url/inputs/create-url-input.ts
@@ -3,7 +3,7 @@ import { getZString } from '../../zod-helpers';
 import rules from '../url.rules';
 
 export const createUrlSchema = z.object({
-	url: getZString('url', rules.url)
+	url: getZString('url', rules.url).url()
 });
 
 export type CreateUrlSchema = typeof createUrlSchema;
