4346750692cd7f163b847c8072ac61f12359210a
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2024-01-26 00:45:41 +0100

    fix: btn width

--- src/lib/components/url-link.svelte
+++ src/lib/components/url-link.svelte
@@ -37,7 +37,7 @@
 		</p>
 		<Button
 			variant="secondary"
-			class="flex aspect-square h-5 items-center justify-center rounded px-1 text-sm font-bold"
+			class="flex aspect-square h-5 w-5 items-center justify-center rounded px-1 text-sm font-bold"
 			on:click={() =>
 				copyToClipboard(
 					href,
