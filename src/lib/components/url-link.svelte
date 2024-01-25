31c52fadabe008a899ae6c06551997238e0b8d46
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2024-01-26 00:12:09 +0100

    made copy secondary

--- src/lib/components/url-link.svelte
+++ src/lib/components/url-link.svelte
@@ -36,7 +36,8 @@
 			{formatCount(url.redirects)}
 		</p>
 		<Button
-			class="flex aspect-square h-5 items-center justify-center rounded bg-primary-foreground px-1 text-sm font-bold text-primary"
+			variant="secondary"
+			class="flex aspect-square h-5 items-center justify-center rounded px-1 text-sm font-bold"
 			on:click={() =>
 				copyToClipboard(
 					href,
