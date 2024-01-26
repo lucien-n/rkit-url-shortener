66a75aa2b3627a723927df74fcb414d0861f45d0
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2024-01-26 18:55:39 +0100

    fix: most viewed urls display on small screen

--- src/lib/components/most-viewed.svelte
+++ src/lib/components/most-viewed.svelte
@@ -8,7 +8,7 @@
 
 <div>
 	<p class="text-center text-lg font-bold">Most viewed urls</p>
-	<div class="grid grid-cols-1 items-center gap-x-8 sm:grid-cols-2">
+	<div class="grid-cols-2 items-center gap-x-8 sm:grid">
 		{#each mostViewedUrls as url, index (url.id)}
 			<div class={cn('mx-auto flex items-center gap-1', index === 2 ? 'col-span-2 mx-auto' : '')}>
 				<UrlLink {url} />
