1a60aacd888d09396be7fa1ec10b7a89c1bf7ecc
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2024-01-26 01:27:00 +0100

    ref: renamed cron secret

--- src/routes/(api)/api/cron/delete-expired/+server.ts
+++ src/routes/(api)/api/cron/delete-expired/+server.ts
@@ -1,9 +1,9 @@
-import { CRON_SECRET } from '$env/static/private';
+import { PRIVATE_CRON_SECRET } from '$env/static/private';
 import { ShortUrlsController } from '$remult/short-url/short-url.controller';
 import type { RequestHandler } from '@sveltejs/kit';
 
 export const GET: RequestHandler = async ({ request }) => {
-	if (request.headers.get('Authorization') !== `Bearer ${CRON_SECRET}`)
+	if (request.headers.get('Authorization') !== `Bearer ${PRIVATE_CRON_SECRET}`)
 		return new Response(null, { status: 403 });
 
 	await ShortUrlsController.deleteExpired();
