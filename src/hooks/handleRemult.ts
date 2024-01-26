5b41d09934c0d236b24e46216fac4521437a504c
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2024-01-26 10:32:40 +0100

    feat: configured remult

--- src/hooks/handleRemult.ts
+++ src/hooks/handleRemult.ts
@@ -1,3 +1,4 @@
+import { dev } from '$app/environment';
 import { PRIVATE_DATABASE_URL } from '$env/static/private';
 import { controllers, entities } from '$remult';
 import { ShortUrl } from '$remult/short-url/short-url.entity';
@@ -7,11 +8,13 @@ import { remultSveltekit } from 'remult/remult-sveltekit';
 export const handleRemult = remultSveltekit({
 	dataProvider: createPostgresDataProvider({ connectionString: PRIVATE_DATABASE_URL }),
 	initApi: async (remult) => {
-		if (process.env.NODE_ENV !== 'PRODUCTION') {
+		if (dev) {
 			const urls = await remult.repo(ShortUrl).find({ include: { credentials: true } });
 			console.table(urls);
 		}
 	},
+	logApiEndPoints: dev,
+	defaultGetLimit: 20,
 	entities,
 	controllers
 });
