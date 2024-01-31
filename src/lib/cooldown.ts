c0d8ed3eb9de71c667fb1314a475798162348506
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2024-01-31 15:58:53 +0100

    removed propagate

--- src/lib/cooldown.ts
+++ src/lib/cooldown.ts
@@ -1,19 +1,21 @@
 /**
  * Disables the button for a certain amnout of time after a certain amount of clicks
  */
-export const cooldown = (button: HTMLButtonElement, { cooldown = 750, afterClicks = 0 } = {}) => {
+export const cooldown = (
+	button: HTMLButtonElement,
+	{ cooldown = 750, afterClicks = 0, onDisable = () => undefined, onEnable = () => undefined } = {}
+) => {
 	let lastClickedAt = 0;
 	let clickedTimes = 0;
 	let enableTimeout = -1;
 
 	const handleClick = (event: MouseEvent) => {
-		clickedTimes += 1;
-
 		if (afterClicks === 0 && !button.disabled) {
 			disable();
 			return;
 		}
 
+		clickedTimes += 1;
 		const now = new Date().getTime();
 		if (now - lastClickedAt < cooldown && !button.disabled && clickedTimes >= afterClicks) {
 			event.preventDefault();
@@ -30,10 +32,12 @@ export const cooldown = (button: HTMLButtonElement, { cooldown = 750, afterClick
 			enable();
 			clickedTimes = 0;
 		}, cooldown);
+		onDisable();
 	};
 
 	const enable = () => {
 		button.disabled = false;
+		onEnable();
 	};
 
 	button.addEventListener('click', handleClick);
