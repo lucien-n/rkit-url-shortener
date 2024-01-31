32e0c79583c775f49645f601b87c55c1486c4af9
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2024-02-01 00:49:40 +0100

    ref: env vars

--- src/hooks/handleRemult.ts
+++ src/hooks/handleRemult.ts
@@ -1,12 +1,12 @@
 import { dev } from '$app/environment';
-import { PRIVATE_DATABASE_URL } from '$env/static/private';
+import { DATABASE_URL } from '$env/static/private';
 import { controllers, entities } from '$remult';
 import { ShortUrl } from '$remult/short-url/short-url.entity';
 import { createPostgresDataProvider } from 'remult/postgres';
 import { remultSveltekit } from 'remult/remult-sveltekit';
 
 export const handleRemult = remultSveltekit({
-	dataProvider: createPostgresDataProvider({ connectionString: PRIVATE_DATABASE_URL }),
+	dataProvider: createPostgresDataProvider({ connectionString: DATABASE_URL }),
 	initApi: async (remult) => {
 		if (dev) {
 			const urls = await remult.repo(ShortUrl).find({ include: { credentials: true } });
--- src/lib/server/redis.ts
+++ src/lib/server/redis.ts
@@ -1,22 +1,23 @@
 import {
-	PRIVATE_ENABLE_RATE_LIMITING,
-	PRIVATE_UPSTASH_REDIS_REST_TOKEN
+	ENABLE_RATE_LIMITING,
+	RL_DEFAULT_TOKENS,
+	UPSTASH_REDIS_REST_TOKEN,
+	UPSTASH_REDIS_REST_URL
 } from '$env/static/private';
-import { PUBLIC_UPSTASH_REDIS_REST_URL } from '$env/static/public';
 import type { NumericRange, RequestEvent } from '@sveltejs/kit';
 import { Ratelimit } from '@upstash/ratelimit';
 import { Redis } from '@upstash/redis';
 
 export const redis = new Redis({
-	url: PUBLIC_UPSTASH_REDIS_REST_URL,
-	token: PRIVATE_UPSTASH_REDIS_REST_TOKEN
+	url: UPSTASH_REDIS_REST_URL,
+	token: UPSTASH_REDIS_REST_TOKEN
 });
 
 export const ratelimit = {
 	default: new Ratelimit({
 		redis,
 		prefix: '@ratelimit/default',
-		limiter: Ratelimit.slidingWindow(1, '60s')
+		limiter: Ratelimit.slidingWindow(parseInt(RL_DEFAULT_TOKENS ?? '3'), '60s')
 	})
 };
 
@@ -24,7 +25,7 @@ export const limit = async (
 	event: RequestEvent,
 	limiter: Ratelimit
 ): Promise<{ message: string; status: NumericRange<400, 599> } | undefined> => {
-	if (PRIVATE_ENABLE_RATE_LIMITING === 'false') return;
+	if (ENABLE_RATE_LIMITING === 'false') return;
 
 	const clientAddress = event.getClientAddress();
 	const { success, reset } = await limiter.limit(clientAddress);
