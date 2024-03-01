24e5c3ef5c706728e1f7e0c1a30c1da7be8fbf66
Lucien <lucien.neuhoff@protonmail.com>
2024-03-01 19:06:41 +0100

    ref: description

--- src/lib/components/seo.svelte
+++ src/lib/components/seo.svelte
@@ -1,7 +1,7 @@
 <script>
 	export let title = 'Shortener';
 	export let description =
-		'Easily shorten long URLs with our web app. Generate concise, shareable links in seconds. Simplify your links with our URL shortening service.';
+		'Easily shorten long URLs. Generate concise, shareable links in seconds.';
 	export let image = 'https://s.lucienn.dev/shortenner.png';
 	export let keywords =
 		'URL shortener, short URLs, link shortening, shorten links, URL generator, link generator, URL shrinker, shortener service, custom short URLs, shorten long URLs, URL abbreviation, link abbreviator, link truncator, URL compressor, link compressor, URL condenser, link condenser, URL simplifier, link simplifier, URL cutter, link cutter, URL trimmer, link trimmer, URL minifier, link minifier';
