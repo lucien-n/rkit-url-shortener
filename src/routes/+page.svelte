5ee779a40ba51dd3f0ab07953251a8551d50fb01
Lucien <lucien.neuhoff@protonmail.com>
2024-03-02 15:44:19 +0100

    adding counter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
+	import Counter from '$components/counter.svelte';
+	import UrlSuccessDialog from '$components/dialogs/url-success-dialog.svelte';
 	import CreateUrlForm from '$components/forms/create-url-form.svelte';
+	import Seo from '$components/seo.svelte';
+	import type { ShortUrl } from '$shared/modules/short-urls/short-url.entity';
 	import { toast } from 'svelte-sonner';
 	import type { PageData } from './$types';
-	import type { ShortUrl } from '$shared/modules/short-urls/short-url.entity';
-	import UrlSuccessDialog from '$components/dialogs/url-success-dialog.svelte';
-	import Seo from '$components/seo.svelte';
 
 	export let data: PageData;
 
@@ -15,8 +16,12 @@
 <Seo title="Shortener" />
 
 <div class=" grid h-full grid-rows-3 md:container">
-	<div class="prose prose-2xl flex w-full flex-col self-center md:self-end">
-		<h1 class="text-6xl md:text-8xl">URL Shortener</h1>
+	<div class="flex w-full flex-col self-center md:self-end">
+		<h1 class="text-6xl font-extrabold tracking-tight md:text-8xl">URL Shortener</h1>
+		<div>
+			<Counter count={2124121224} />
+			<h3 class="px-3 text-2xl text-muted-foreground">urls shortened</h3>
+		</div>
 	</div>
 	<div class="h-fit w-full self-center md:w-3/4">
 		<CreateUrlForm
